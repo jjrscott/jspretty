9d4cc74af9f98c30283def947c749b11ccd5eb1b
John Scott <john@jjrscott.com>
2023-06-02 16:28:21 +0100

    Allow disabling of object inlining for large objects

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [jq](https://jqlang.github.io/jq/) does a great job of making JSON data readable. However for large and repetitive data some improvements can be made, eg inlining objects and arrays that only contain primitives and ordering keys by numeric value.
 
-### Example
+## Example
 
 #### Compact
 
@@ -37,4 +37,17 @@
   "en10": [ "Hello", "World" ],
   "fr": [ "Bonjour", "Monde" ]
 }
+```
+
+## Usage
+
+```
+usage: jspretty.py [-h] [-max-inline-length MAX_INLINE_LENGTH]
+
+Format JSON for improved ledgability
+
+options:
+  -h, --help            show this help message and exit
+  --max-inline-length MAX_INLINE_LENGTH
+                        Maximum length of inlined objects and arrays
 ```
--- jspretty.py
+++ jspretty.py
@@ -1,33 +1,40 @@
 import json
 import re
 import sys
-
+import argparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Format JSON for improved ledgability")
+    parser.add_argument('--max-inline-length', default=120, type=int, help="Maximum length of inlined objects and arrays")
+
+    args = parser.parse_args()
+
     root = json.loads(sys.stdin.read())
+    print(encode(root, maxInlineLength=args.max_inline_length))
 
-    print(encode(root))
-
-def encode(node, *nodes):
+def encode(node, *nodes, maxInlineLength):
     if isinstance(node, list):
         allPrimitaves = True
         for element in node:
             if isinstance(element, list) or isinstance(element, dict):
                 allPrimitaves = False
         if allPrimitaves:
-            return '[ ' + ', '.join(map(lambda element: encode(element, node, *nodes), node))+ ' ]'
-        else:
-            return '[\n' + ',\n'.join(map(lambda element: indent(element, node, *nodes)+encode(element, node, *nodes), node))+ '\n'+indent(node, *nodes) + ']'
+            text = '[ ' + ', '.join(map(lambda element: encode(element, node, *nodes, maxInlineLength=maxInlineLength), node))+ ' ]'
+            if len(text) < maxInlineLength:
+                return text
+
+        return '[\n' + ',\n'.join(map(lambda element: indent(element, node, *nodes)+encode(element, node, *nodes, maxInlineLength=maxInlineLength), node))+ '\n'+indent(node, *nodes) + ']'
     elif isinstance(node, dict):
         allPrimitaves = True
         for value in node.values():
             if isinstance(value, list) or isinstance(value, dict):
                 allPrimitaves = False
-        maxKeyLength = max(map(len, node))
         if allPrimitaves:
-            return '{ ' + ', '.join(map(lambda item: encode(item[0], node, *nodes)+': '+encode(item[1], node, *nodes), sorted(node.items(), key=objectKey)))  + ' }'
-        else:
-            return '{\n' + ',\n'.join(map(lambda item: encodeItem(item[0], item[1], maxKeyLength, node, *nodes), sorted(node.items(), key=objectKey)))  + '\n'+indent(node, *nodes) +'}'
+            text = '{ ' + ', '.join(map(lambda item: encode(item[0], node, *nodes, maxInlineLength=maxInlineLength)+': '+encode(item[1], node, *nodes, maxInlineLength=maxInlineLength), sorted(node.items(), key=objectKey)))  + ' }'
+            if len(text) < maxInlineLength:
+                return text
+
+        return '{\n' + ',\n'.join(map(lambda item: encodeItem(item[0], item[1], node, *nodes, maxInlineLength=maxInlineLength), sorted(node.items(), key=objectKey)))  + '\n'+indent(node, *nodes) +'}'
     elif isinstance(node, str):
         return '"'+node+'"'
     elif isinstance(node, bool):
@@ -38,8 +45,8 @@ def encode(node, *nodes):
         # exit(type(node))
         return str(node)
 
-def encodeItem(key, value, maxKeyLength, node, *nodes):
-    return indent(key, node, *nodes)+encode(key, node, *nodes)+': '+encode(value, node, *nodes)
+def encodeItem(key, value, node, *nodes, maxInlineLength):
+    return indent(key, node, *nodes)+encode(key, node, *nodes, maxInlineLength=maxInlineLength)+': '+encode(value, node, *nodes, maxInlineLength=maxInlineLength)
 
 def indent(node, *nodes):
     return "  " * len(nodes)
